95d46b896b6aaef714e13654160496237ee75dda
nishibaiyang <693482496@qq.com>
2019-03-05 15:21:52 +0800

    修改华为手机刘海屏、全面屏出现的bug

--- keyboard/src/main/java/cn/hadcn/keyboard/view/SoftListenLayout.java
+++ keyboard/src/main/java/cn/hadcn/keyboard/view/SoftListenLayout.java
@@ -5,8 +5,11 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.ViewTreeObserver;
 import android.widget.RelativeLayout;
+
+import java.lang.reflect.Field;
 
 import cn.hadcn.keyboard.utils.Utils;
 
@@ -21,6 +24,12 @@ public abstract class SoftListenLayout extends RelativeLayout {
     private int mGlobalBottom = 0;
     private int mKeyboardHeight = 0;
     protected Context mContext;
+    // 状态栏的高度
+    private int statusBarHeight;
+    // 底部虚拟按键的高度
+    private int softButtonsBarHeight;
+    // 软键盘的显示状态
+    private boolean isShowKeyboard;
 
     public SoftListenLayout(final Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -34,6 +43,9 @@ public abstract class SoftListenLayout extends RelativeLayout {
 
         mKeyboardHeight = Utils.getDefKeyboardHeight(mContext);
 
+        statusBarHeight = getStatusBarHeight(mContext);
+        softButtonsBarHeight = getSoftButtonsBarHeight((Activity) mContext);
+
         getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver
                 .OnGlobalLayoutListener() {
             @Override
@@ -41,17 +53,28 @@ public abstract class SoftListenLayout extends RelativeLayout {
                 Rect r = new Rect();
                 ((Activity) getContext()).getWindow().getDecorView()
                         .getWindowVisibleDisplayFrame(r);
-                int popHeight = Utils.getDisplayHeightPixels(context) - r.bottom;
-                if ((mGlobalBottom != 0 && mGlobalBottom - r.bottom > mMinKeyboardHeight)
-                        || (popHeight != 0 && mKeyboardHeight != popHeight)) {
-                    // keyboard pop
-                    mKeyboardHeight = popHeight;
-                    onSoftKeyboardPop(mKeyboardHeight);
-                } else if (mGlobalBottom != 0 && r.bottom - mGlobalBottom > mMinKeyboardHeight) {
-                    onSoftKeyboardClose();
+
+                int screenHeight = ((Activity) getContext()).getWindow().getDecorView().getRootView().getHeight();
+                int heightDiff = screenHeight - (r.bottom -r.top);
+                if(mKeyboardHeight == 0 && heightDiff > statusBarHeight + softButtonsBarHeight){
+                    mKeyboardHeight = heightDiff -statusBarHeight -softButtonsBarHeight;
                 }
 
-                mGlobalBottom = r.bottom;
+                if (isShowKeyboard) {
+                    // 如果软键盘是弹出的状态，并且heightDiff小于等于 状态栏 + 虚拟按键 高度，
+                    // 说明这时软键盘已经收起
+                    if (heightDiff <= statusBarHeight + softButtonsBarHeight) {
+                        isShowKeyboard = false;
+                        OnSoftKeyboardClose();
+                    }
+                } else {
+                    // 如果软键盘是收起的状态，并且heightDiff大于 状态栏 + 虚拟按键 高度，
+                    // 说明这时软键盘已经弹出
+                    if (heightDiff > statusBarHeight + softButtonsBarHeight) {
+                        isShowKeyboard = true;
+                        OnSoftKeyboardPop(mKeyboardHeight);
+                    }
+                }
             }
         });
     }
@@ -70,7 +93,38 @@ public abstract class SoftListenLayout extends RelativeLayout {
         }
     }
 
-    protected abstract void onSoftKeyboardPop(int height);
+    /**
+     * 获取状态栏高度
+     */
+    public static int getStatusBarHeight(Context context) {
+        try {
+            Class<?> c = Class.forName("com.android.internal.R$dimen");
+            Object obj = c.newInstance();
+            Field field = c.getField("status_bar_height");
+            int x = Integer.parseInt(field.get(obj).toString());
+            return context.getResources().getDimensionPixelSize(x);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 
-    protected abstract void onSoftKeyboardClose();
+    /**
+     * 获取底部虚拟按键的高度
+     */
+    public static int getSoftButtonsBarHeight(Activity activity) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int usableHeight = metrics.heightPixels;
+        activity.getWindowManager().getDefaultDisplay().getRealMetrics(metrics);
+        int realHeight = metrics.heightPixels;
+        if (realHeight > usableHeight)
+            return realHeight - usableHeight;
+        else
+            return 0;
+    }
+
+    protected abstract void OnSoftKeyboardPop(int height);
+
+    protected abstract void OnSoftKeyboardClose();
 }
